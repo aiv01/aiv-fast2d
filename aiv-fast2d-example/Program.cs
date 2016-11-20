1f3f549e4e738861f16b22778cb260b5929d30c5
Roberto De Ioris <roberto@20tab.com>
2016-11-20 19:01:04 +0100

    added example code

---
+++ aiv-fast2d-example/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using OpenTK;
+
+namespace Aiv.Fast2D.Example
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Window window = new Window(800, 600, "Aiv.Fast2D.Example");
+
+            Texture logoAiv = new Texture("aiv_fast2d_example.Assets.LogoAIV.png");
+
+
+            Texture alien = new Texture("aiv_fast2d_example.Assets.owl.png");
+
+
+            Sprite logo = new Sprite(logoAiv.Width, logoAiv.Height);
+
+            int height = 150;
+
+            Sprite ship = new Sprite(alien.Width / 10, height);
+
+            Sprite ship2 = new Sprite(alien.Width / 10, height);
+
+            RenderTexture screen = new RenderTexture(800, 600);
+
+            Sprite monitor = new Sprite(100, 100);
+            monitor.position = new Vector2(400, 200);
+
+            int index = 0;
+            float t = 0;
+
+            window.SetClearColor(100, 100, 100);
+
+            while (window.opened)
+            {
+
+                ship.position.Y = 10;
+                ship.position += new Vector2(5f, 0) * window.deltaTime;
+
+                ship.scale = new Vector2(1f, 1f);
+
+                t += window.deltaTime;
+                if (t > 1f / 24f)
+                {
+                    index++;
+                    if (index >= 51)
+                        index = 0;
+                    t = 0;
+                }
+                int x = (index % 10) * (alien.Width / 10);
+                int y = (index / 10) * height;
+                
+
+                ship.DrawTexture(alien, x, y, alien.Width / 10, height);
+
+                window.SetClearColor(255, 0, 0);
+                RenderTexture.To(screen);
+
+                logo.position.Y = 100;
+                logo.position += new Vector2(50f, 0) * window.deltaTime;
+                logo.scale = new Vector2(1f, 1f);
+                logo.DrawTexture(logoAiv);
+
+                if (window.GetKey(KeyCode.Esc))
+                    break;
+
+                window.SetClearColor(100, 100, 100);
+                RenderTexture.To(null);
+
+              
+                monitor.DrawTexture(screen);
+
+                ship2.position = new Vector2(300, 300);
+                ship2.DrawTexture(alien, x, y, alien.Width / 10, height);
+
+
+
+                window.Update();
+            }
+        }
+    }
+}
---
+++ aiv-fast2d/RenderTexture.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL;
+
+
+namespace Aiv.Fast2D
+{
+    public class RenderTexture : Texture
+    {
+        private int frameBuffer;
+
+        public RenderTexture(int width, int height) : base(width, height)
+        {
+            frameBuffer = GL.GenFramebuffer();
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, frameBuffer);
+            GL.FramebufferTexture(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, this.Id, 0);
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+            this.flipped = true;
+        }
+
+        public static void To(RenderTexture renderTexture, bool clear = true)
+        {
+            if (renderTexture == null)
+            {
+                GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+                return;
+            }
+            else {
+                GL.BindFramebuffer(FramebufferTarget.Framebuffer, renderTexture.frameBuffer);
+            }
+
+            if (clear)
+            {
+                GL.Clear(ClearBufferMask.ColorBufferBit);
+            }
+        }
+    }
+}
