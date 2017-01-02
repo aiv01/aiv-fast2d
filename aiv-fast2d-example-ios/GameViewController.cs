9c1940f0b8c09ec11b06c90a226a9e0d030fcbbb
Roberto De Ioris <roberto@20tab.com>
2017-01-02 14:36:22 +0100

    preliminary implementation of LoadImage for iOS

--- aiv-fast2d-example-ios/GameViewController.cs
+++ aiv-fast2d-example-ios/GameViewController.cs
@@ -6,6 +6,7 @@ using GLKit;
 using OpenGLES;
 
 using Aiv.Fast2D.iOS;
+using OpenTK;
 
 namespace Aiv.Fast2D.Example.iOS
 {
@@ -13,6 +14,7 @@ namespace Aiv.Fast2D.Example.iOS
 	public class GameViewController : MobileGame
 	{
 		Sprite sprite;
+		Texture alien;
 
 		[Export("initWithCoder:")]
 		public GameViewController(NSCoder coder) : base(coder)
@@ -23,13 +25,21 @@ namespace Aiv.Fast2D.Example.iOS
 		{
 			window.SetClearColor(1f, 0, 0, 1f);
 			sprite = new Sprite(100, 100);
+
+			alien = new Texture("2.png");
 		}
 
 		protected override void GameUpdate(Window window)
 		{
+			sprite.scale = Vector2.One;
 			sprite.position = window.TouchPosition;
 
 			sprite.DrawSolidColor(0f, 1f, 1f, 1f);
+
+			sprite.scale = new Vector2(2, 2);
+			sprite.position = Vector2.Zero;
+
+			sprite.DrawTexture(alien);
 		}
 	}
 }
