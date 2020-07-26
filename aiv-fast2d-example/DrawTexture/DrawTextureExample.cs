edd7da0614c2f3e6f5fd25ba78b5efcf44fd77dd
Federico De Felici <fdf8210@gmail.com>
2020-07-26 11:45:02 +0200

    #56 merged all examples in one console project that allow selection

---
+++ aiv-fast2d-example/DrawTexture/DrawTextureExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aiv.Fast2D;
+
+namespace Aiv.Fast2D.Example
+{
+    static class DrawTextureExample
+    {
+        public static void Run()
+        {
+            Window window = new Window(800, 600, "texture");
+
+            Texture aj = new Texture("DrawTexture/Assets/aj.jpg");
+            Sprite sprite = new Sprite(300, 300);
+
+            while (window.IsOpened)
+            {
+                sprite.DrawTexture(aj);
+                window.Update();
+            }
+        }
+    }
+}
